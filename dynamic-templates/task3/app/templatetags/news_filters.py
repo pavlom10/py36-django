158c3320ca7e37cdb150b035c4e098d3b15ed7b4
pavlom <10door@gmail.com>
2021-05-13 19:57:10 +0300

    Task 3

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from datetime import datetime
 
 
 register = template.Library()
@@ -6,16 +7,53 @@ register = template.Library()
 
 @register.filter
 def format_date(value):
-    # Ваш код
+    now = datetime.now().timestamp()
+    sec_ago = now - value
+
+    if sec_ago < 10 * 60:
+        value = 'Только что'
+    elif sec_ago < 24 * 60 * 60:
+        hour = round(sec_ago / 60 / 60)
+        value = str(hour) + ' часов назад'
+    else:
+        date_time = datetime.fromtimestamp(value)
+        value = date_time.strftime("%Y-%m-%d")
+
     return value
 
 
-# необходимо добавить фильтр для поля `score`
+@register.filter
+def format_score(value, default):
+    if value is None:
+        return default
+
+    if value < -5:
+        value = 'Все плохо'
+    elif -5 <= value < 5:
+        value = 'Нейтрально'
+    elif value > 5:
+        value = 'Хорошо'
+
+    return value
 
 
 @register.filter
 def format_num_comments(value):
-    # Ваш код
+    if value == 0:
+        value = 'Оставьте комментарий'
+    elif value > 50:
+        value = '50+'
+
+    return value
+
+
+@register.filter
+def format_selftext(value, count):
+    words = value.split()
+    if len(words) > count + count:
+        s = ' '.join(words[:count]) + ' ... ' + ' '.join(words[-count:])
+        return s
+
     return value
 
 
