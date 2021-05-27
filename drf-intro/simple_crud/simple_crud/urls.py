173ccd982e2e8706fe08df5b1fff20a564611952
pavlom <10door@gmail.com>
2021-05-27 15:58:18 +0300

    Urls

--- drf-intro/simple_crud/simple_crud/urls.py
+++ drf-intro/simple_crud/simple_crud/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from measurements.views import ProjectViewSet, MeasurementViewSet
 from rest_framework.routers import DefaultRouter
 
+from measurements.views import ProjectViewSet, MeasurementViewSet
 
 router = DefaultRouter()
 router.register(r'project', ProjectViewSet, basename='project')
